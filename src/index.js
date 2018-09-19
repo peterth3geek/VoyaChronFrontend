f2177223e481455ed1062b09e11e088f2344cce4
github username <github email address>
2018-09-19 18:01:10 -0400

    failed attempt at setting up redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  user: {},
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    default:
+    return state
+  }
+}
